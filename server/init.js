1e0325b3489459cd5b53ae75ceef35b711b00490
Raúl Torres González <r.torresgonzalez.93@gmail.com>
2017-02-26 03:59:31 +0100

    primer commit

---
+++ server/init.js
@@ -0,0 +1,11 @@
+// configuration    =============================================================================================================================
+  // Load required modules
+  var http        = require('http');    // http server core module
+  var express     = require('express'); // web framework external module
+  var httpApp     = express();
+
+  // Start Express http server on port 5050
+  var webServer = http.createServer(httpApp).listen(5050);
+
+  // Setup and configure Express http server. Expect a subfolder called "public" to be the web root.
+  httpApp.use("/", express.static("../public"));
