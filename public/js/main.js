725978ae028e2b62071ae078f8b84e21512bec90
Linkmetal <linkmetal18@gmail.com>
2017-03-01 05:26:55 +0000

    --Agregada la funcion de leer desde fichero un mapa, junto con su contenido (Boton "Load")
    --Ahora la pestaña muestra el nombre del archivo
    
    --Arreglado el problema de resolución de la ventana "Create Map"

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
-function showCreateMapWindow(){
 
+function showLoadMapWindow(){
+	$("#tabs").append("<div class='tab'>Map001Tab</div>");
+	$("#mapDiv").append("<div id='loadMapWindow' height= '200px' width= '400px'>");
+	$("#loadMapWindow").append("<input type='file' id='files' name='files[]' multiple />");
+	$("#loadMapWindow").append("<output id='filelist'></output>");
+	document.getElementById('files').addEventListener("change",handleFileSelect,false);
+}
+
+function showCreateMapWindow(){
 	$("#tabs").append("<div class='tab'>Map001Tab</div>");
 	$("#mapDiv").append("<div id='createMapWindow' height= '200px' width= '400px'>");
 	$("#createMapWindow").append("</br></br>Name: <input type='text' name='name' value=''>");
@@ -15,6 +23,7 @@ function createMap(){
 	var columns = $('input[name="columns"]').val();
 	var spriteSize = $('input[name="size"]').val();
 
+	$(".tab:last-child").html(name);
 	$("#createMapWindow").remove();
 	$("#mapDiv").append("<div class='map' id='" + name + "' height= '90%' width= '90%'>");
 
@@ -24,9 +33,7 @@ function createMap(){
 	str += "<table>";
 	for(var i = 0; i < rows; i++){
 		str += "<tr>";
-		//$("#" + name).append("<tr>")
 		for(var j = 0; j < columns; j++){
-				//$("#" + name).append("<td></td>")
 				str += "<td height='";
 				str += spriteSize;
 				str += "px' width='"
@@ -34,9 +41,42 @@ function createMap(){
 				str += "px'></td>";
 		}
 		str += "</tr>";
-	//	$("#" + name).append("</tr>")
 	}
 	str += "</table>";
 	$("#" + name).append(str);
+}
 
+
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+	var f = files[0];
+	$(".tab:last-child").html(f.name);
+	var reader = new FileReader();
+	reader.readAsText(f);
+	reader.addEventListener("load",createMapFromFile,false);
+}
+
+function createMapFromFile(e){
+	var	output = e.target.result;
+	var mapArray = output.split("\n");
+
+	var name = $(".tabs:last-child").html();
+	$("#loadMapWindow").remove();
+	$("#mapDiv").append("<div class='map' id='" + name + "' height= '90%' width= '90%'>");
+
+	var map = document.getElementById(name);
+	var str = '';
+
+	str += "<table>";
+ 	for(var i = 0; i < mapArray.length-1; i++){
+ 		str += "<tr>";
+ 		for(var j = 0; j < mapArray[i].length-1; j++){
+			str += "<td>" + mapArray[i][j] + "</td>";
+		}
+ 		str += "</tr>";
+	}
+
+ 	str += "</table>";
+ 	$("#" + name).append(str);
 }
